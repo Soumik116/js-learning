2189e3da349917f3d1111a8dc4aaa2e335f109c1
Soumik116 <144416110+Soumik116@users.noreply.github.com>
2023-09-12 20:11:10 +0000

    comparision and datatype summary done

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 = 1);
+// console.log(2 != 1); these type of comparisions are good
+
+
+ console.log("2" > 1); //we should avoid these type of comparisions
+ console.log("02" > 1);
+
+ console.log(null > 0); //we should avoid these type of comparisions
+ console.log(null == 0);
+ console.log(null >= 0);
+
+ console.log(undefined == 0); //we should avoid these type of comparisions
+ console.log(undefined > 0);
+ console.log(undefined < 0);
+
+//===
+
+console.log("2" === 2);
---
+++ datatypes-summary.js
@@ -0,0 +1,40 @@
+//There are mainly two types of datatype on the basis of how we store the data and how we acess the data which are primitive and Non-primitive (or,reference type) data
+
+// Primitive
+
+// 7 type : String, Number, Boolean, null, undefined, symbol, BigInt
+
+//Javascript is a dynamically typed language
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+// Reference (Non-primitive)
+
+// Arrays, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
